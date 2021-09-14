6b63cd92fb168c8002d213bd5667bf510bb6a33e
Artem Denysov <denysov.artem@gmail.com>
2021-09-14 14:11:45 +0300

    reformat netlify func

--- .netlify/functions/submission-created.js
+++ .netlify/functions/submission-created.js
@@ -4,34 +4,34 @@ const axios = require('axios');
 
 // Handle the lambda invocation
 exports.handler = async function (event, context, callback) {
-  try {
-    const url = process.env.STACKBIT_API_URL;
-    const projectId = process.env.STACKBIT_PROJECT_ID;
+    try {
+        const url = process.env.STACKBIT_API_URL;
+        const projectId = process.env.STACKBIT_PROJECT_ID;
 
-    if (!url) {
-      throw new Error('No Stackbit URL specified');
+        if (!url) {
+            throw new Error('No Stackbit URL specified');
+        }
+        if (!url) {
+            throw new Error('No Stackbit project id specified');
+        }
+
+        const response = await axios({
+            method: 'post',
+            url,
+            data: {
+                projectId,
+                ...JSON.parse(event.body)
+            }
+        });
+
+        callback(null, {
+            statusCode: 200,
+            body: response.data.status
+        });
+    } catch (e) {
+        callback(null, {
+            statusCode: e?.response?.status ?? 500,
+            body: e?.response?.statusText ?? e.message
+        });
     }
-    if (!url) {
-      throw new Error('No Stackbit project id specified');
-    }
-
-    const response = await axios({
-      method: 'post',
-      url,
-      data: {
-        projectId,
-        ...JSON.parse(event.body)
-      }
-    });
-
-    callback(null, {
-      statusCode: 200,
-      body: response.data.status
-    });
-  } catch (e) {
-    callback(null, {
-      statusCode: e?.response?.status ?? 500,
-      body: e?.response?.statusText ?? e.message
-    });
-  }
 };
