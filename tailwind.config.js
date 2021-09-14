b4f307aa72feb37c025ded31ea7ccdf7c7f754aa
Robert Austin <rob@jugglerdigital.com>
2021-09-14 15:12:25 +1000

    move @apply to scss fle. fix purge. explore tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,32 @@ const plugin = require('tailwindcss/plugin');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    presets: [require('@stackbit/components/themes/tailwind.bold.config')],
     mode: 'jit',
     purge: {
-        enabled: true,
-        content: [
-            './src/**/*.{js,ts,jsx,tsx}',
-            './node_modules/@stackbit/components/themes/*.{js,ts,jsx,tsx}',
-            './node_modules/@stackbit/components/{base,layouts,components}/**/*.{js,ts,jsx,tsx}'
-        ]
+        content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@stackbit/components/{base,layouts,components}/**/*.{js,ts,jsx,tsx}'],
+        safelist: ['colors-a', 'colors-b', 'colors-c', 'colors-d', 'colors-e', 'colors-f']
     },
-    darkMode: false, // or 'media' or 'class'
+    darkMode: false,
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: '#FFC329',
+                secondary: '#FFF7E3',
+                base: '#262626',
+                'complimentary-1': '#e8f3ee',
+                'complimentary-2': '#e2e4ff',
+                info: '#EA5234'
+            },
+            fontFamily: {
+                sans: ['Poppins', ...defaultTheme.fontFamily.serif]
+            },
+            spacing: {
+                '1/1': '100%',
+                '1/3': '33.333%',
+                '1/2': '50%',
+                '2/3': '66.667%'
+            }
+        }
     },
     variants: {
         extend: {}
