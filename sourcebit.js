4e5e76c0b3c8a5bd61d90e1b1c90a979002f6519
Robert Austin <rob@jugglerdigital.com>
2021-09-14 13:55:35 +1000

    change linter to 4 spaces to align with all other repos

--- sourcebit.js
+++ sourcebit.js
@@ -3,66 +3,66 @@ const { cssClassesFromFilePath, urlPathFromFilePath } = require('./src/utils/pat
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  plugins: [
-    {
-      module: require('sourcebit-source-filesystem'),
-      options: {
-        watch: isDev,
-        sources: [
-          { name: 'pages', path: path.join(__dirname, 'content/pages') },
-          { name: 'data', path: path.join(__dirname, 'content/data') }
-        ]
-      }
-    },
-    flattenMarkdownData(),
-    resolveReferenceFields(),
-    {
-      module: require('sourcebit-target-next'),
-      options: {
-        liveUpdate: isDev,
-        flattenAssetUrls: true,
-        pages: (data) => {
-          const pages = data.filter((page) => page.__metadata.sourceName === 'pages');
-          const site = data.find((page) => page.__metadata.id === 'content/data/config.json');
-          return pages.map((page) => {
-            const path = urlPathFromFilePath(page.__metadata.relSourcePath);
-            const pageCssClasses = cssClassesFromFilePath(page.__metadata.relSourcePath);
-            return {
-              path,
-              site,
-              page: {
-                ...page,
-                __metadata: {
-                  ...page.__metadata,
-                  pageCssClasses
+    plugins: [
+        {
+            module: require('sourcebit-source-filesystem'),
+            options: {
+                watch: isDev,
+                sources: [
+                    { name: 'pages', path: path.join(__dirname, 'content/pages') },
+                    { name: 'data', path: path.join(__dirname, 'content/data') }
+                ]
+            }
+        },
+        flattenMarkdownData(),
+        resolveReferenceFields(),
+        {
+            module: require('sourcebit-target-next'),
+            options: {
+                liveUpdate: isDev,
+                flattenAssetUrls: true,
+                pages: (data) => {
+                    const pages = data.filter((page) => page.__metadata.sourceName === 'pages');
+                    const site = data.find((page) => page.__metadata.id === 'content/data/config.json');
+                    return pages.map((page) => {
+                        const path = urlPathFromFilePath(page.__metadata.relSourcePath);
+                        const pageCssClasses = cssClassesFromFilePath(page.__metadata.relSourcePath);
+                        return {
+                            path,
+                            site,
+                            page: {
+                                ...page,
+                                __metadata: {
+                                    ...page.__metadata,
+                                    pageCssClasses
+                                }
+                            }
+                        };
+                    });
                 }
-              }
-            };
-          });
+            }
         }
-      }
-    }
-  ]
+    ]
 };
 
 function flattenMarkdownData() {
-  return ({ data }) => {
-    const objects = data.objects.map((object) => {
-      if ('frontmatter' in object) {
-        return {
-          __metadata: object.__metadata,
-          ...object.frontmatter,
-          markdown_content: object.markdown || null
-        };
-      }
-      return object;
-    });
+    return ({ data }) => {
+        const objects = data.objects.map((object) => {
+            if ('frontmatter' in object) {
+                return {
+                    __metadata: object.__metadata,
+                    ...object.frontmatter,
+                    markdown_content: object.markdown || null
+                };
+            }
+            return object;
+        });
 
-    return {
-      ...data,
-      objects
+        return {
+            ...data,
+            objects
+        };
     };
-  };
 }
 
 /**
@@ -83,58 +83,58 @@ function flattenMarkdownData() {
  * @param {number} options.maxDepth Maximum depth of references to resolve. Default 2.
  */
 function resolveReferenceFields({ fieldNames = [], maxDepth = 2 } = {}) {
-  return ({ data }) => {
-    const objectsByFilePath = data.objects.reduce((map, object) => {
-      map[object.__metadata.id] = object;
-      return map;
-    }, {});
+    return ({ data }) => {
+        const objectsByFilePath = data.objects.reduce((map, object) => {
+            map[object.__metadata.id] = object;
+            return map;
+        }, {});
 
-    const objects = data.objects.map((object) => {
-      let refKeyPathStack = [];
-      return mapDeep(object, (value, keyPath) => {
-        if (keyPath.includes('__metadata')) {
-          return value;
-        }
-        if (fieldNames.length !== 0 && !fieldNames.includes(keyPath[keyPath.length - 1])) {
-          return value;
-        }
-        if (typeof value !== 'string') {
-          return value;
-        }
-        if (!/\.(?:md|mdx|json|yml|yaml|toml)$/.test(value)) {
-          return value;
-        }
-        const keyPathStr = keyPath.join('.');
-        while (refKeyPathStack.length && !keyPathStr.startsWith(refKeyPathStack[refKeyPathStack.length - 1])) {
-          refKeyPathStack.pop();
-        }
-        if (refKeyPathStack.length > maxDepth) {
-          return value;
-        }
-        if (value in objectsByFilePath) {
-          refKeyPathStack.push(keyPath.join('.'));
-          return objectsByFilePath[value];
-        }
-        return value;
-      });
-    });
+        const objects = data.objects.map((object) => {
+            let refKeyPathStack = [];
+            return mapDeep(object, (value, keyPath) => {
+                if (keyPath.includes('__metadata')) {
+                    return value;
+                }
+                if (fieldNames.length !== 0 && !fieldNames.includes(keyPath[keyPath.length - 1])) {
+                    return value;
+                }
+                if (typeof value !== 'string') {
+                    return value;
+                }
+                if (!/\.(?:md|mdx|json|yml|yaml|toml)$/.test(value)) {
+                    return value;
+                }
+                const keyPathStr = keyPath.join('.');
+                while (refKeyPathStack.length && !keyPathStr.startsWith(refKeyPathStack[refKeyPathStack.length - 1])) {
+                    refKeyPathStack.pop();
+                }
+                if (refKeyPathStack.length > maxDepth) {
+                    return value;
+                }
+                if (value in objectsByFilePath) {
+                    refKeyPathStack.push(keyPath.join('.'));
+                    return objectsByFilePath[value];
+                }
+                return value;
+            });
+        });
 
-    return {
-      ...data,
-      objects
+        return {
+            ...data,
+            objects
+        };
     };
-  };
 }
 
 function mapDeep(value, iteratee, _keyPath = [], _objectStack = []) {
-  value = iteratee(value, _keyPath, _objectStack);
-  if (value && typeof value == 'object' && value.constructor === Object) {
-    value = Object.entries(value).reduce((res, [key, val]) => {
-      res[key] = mapDeep(val, iteratee, _keyPath.concat(key), _objectStack.concat(value));
-      return res;
-    }, {});
-  } else if (Array.isArray(value)) {
-    value = value.map((val, key) => mapDeep(val, iteratee, _keyPath.concat(key), _objectStack.concat(value)));
-  }
-  return value;
+    value = iteratee(value, _keyPath, _objectStack);
+    if (value && typeof value == 'object' && value.constructor === Object) {
+        value = Object.entries(value).reduce((res, [key, val]) => {
+            res[key] = mapDeep(val, iteratee, _keyPath.concat(key), _objectStack.concat(value));
+            return res;
+        }, {});
+    } else if (Array.isArray(value)) {
+        value = value.map((val, key) => mapDeep(val, iteratee, _keyPath.concat(key), _objectStack.concat(value)));
+    }
+    return value;
 }
--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -4,28 +4,28 @@ import { withRemoteDataUpdates } from 'sourcebit-target-next/with-remote-data-up
 import { getDynamicComponent } from '@stackbit/components/components-registry';
 
 function Page(props) {
-  const { page, site } = props;
-  const { layout } = page;
+    const { page, site } = props;
+    const { layout } = page;
 
-  if (!layout) {
-    throw new Error(`page has no layout, page '${props.path}'`);
-  }
-  const PageLayout = getDynamicComponent(layout);
-  if (!PageLayout) {
-    throw new Error(`no page layout matching the layout: ${layout}`);
-  }
-  return <PageLayout page={page} site={site} />;
+    if (!layout) {
+        throw new Error(`page has no layout, page '${props.path}'`);
+    }
+    const PageLayout = getDynamicComponent(layout);
+    if (!PageLayout) {
+        throw new Error(`no page layout matching the layout: ${layout}`);
+    }
+    return <PageLayout page={page} site={site} />;
 }
 
 export async function getStaticPaths() {
-  let data = await sourcebitDataClient.getData();
-  const paths = data.pages.map((page) => page.path);
-  return { paths, fallback: false };
+    let data = await sourcebitDataClient.getData();
+    const paths = data.pages.map((page) => page.path);
+    return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
-  const props = await sourcebitDataClient.getStaticPropsForPageAtPath(params.slug);
-  return { props };
+    const props = await sourcebitDataClient.getStaticPropsForPageAtPath(params.slug);
+    return { props };
 }
 
 export default withRemoteDataUpdates(Page);
