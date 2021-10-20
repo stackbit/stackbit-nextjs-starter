b7998f599b6d05aaa9da051f9f3009044775157c
Robert Austin <rob@jugglerdigital.com>
2021-10-20 17:28:38 +1000

    update tailwind config for theme pipeline"

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const sourcebit = require('sourcebit');
 const sourcebitConfig = require('./sourcebit.js');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,19 @@
 const plugin = require('tailwindcss/plugin');
 const defaultTheme = require('tailwindcss/defaultTheme');
-const themeStyle = require('./content/data/theme-style.json');
+// require style
 
 module.exports = {
-    presets: [require('@stackbit/components/styles/default/tailwind.default.config')],
+    presets: [require('@stackbit/components/styles/default/tailwind.default.config.js')],
     mode: 'jit',
     purge: {
         enabled: true,
-        content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@stackbit/components/src/{base,layouts,components}/**/*.{js,ts,jsx,tsx}', './content/**' /* for ThemeStyle object */],
+        content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@stackbit/components/src/{base,layouts,components}/**/*.{js,ts,jsx,tsx}', './content/**'],
         safelist: ['colors-a', 'colors-b', 'colors-c', 'colors-d', 'colors-e', 'colors-f', 'colors-g', 'colors-h', 'colors-i']
     },
     darkMode: false,
     theme: {
         extend: {
-            colors: {
-                body: themeStyle.body,
-                headline: themeStyle.headline,
-                primary: themeStyle.primary,
-                secondary: themeStyle.secondary,
-                neutral: themeStyle.neutral,
-                complementary: themeStyle.complementary,
-                'complementary-alt': themeStyle['complementary-alt'],
-                info: themeStyle.info,
-                success: themeStyle.success,
-                warning: themeStyle.warning
-            }
+            // ...style
         }
     },
     variants: {
