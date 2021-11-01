89e26b5d015ad388784839dbd433193e73be2c3a
Robert Austin <rob@jugglerdigital.com>
2021-11-01 13:24:07 +1000

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import '../components/register-components';
 import '../css/main.css';
+import Head from 'next/head';
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.svg" />
+            </Head>
+            <Component {...pageProps} />;
+        </>
+    );
 }
